cc98869fced96d2d858f89474e6cb1cab5018d6e
L-R-C-C-M <liuruimeng291@163.com>
2022-12-15 21:57:32 +0800

    修正token

--- src/main/java/com/yixun/yixun_backend/config/InterceptorConfig.java
+++ src/main/java/com/yixun/yixun_backend/config/InterceptorConfig.java
@@ -14,8 +14,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtInterceptor())
                 .addPathPatterns("/**")  // 拦截所有请求，通过判断token是否合法来决定是否需要登录
                 .excludePathPatterns("/api/LogIn/**", "/api/MainPage/**", "/api/News/**", "/api/RelatedDp/**", "/api/SearchInfo/**",
-                        "/api/VolAct/**", "/api/Vol/ShowTenVolunteer")//放行的接口，应该填入无需登录也可以用的接口
-                .excludePathPatterns( "/**/*.html", "/**/*.js", "/**/*.css", "/**/*.woff", "/**/*.ttf"); // 放行静态文件
+                        "/api/VolAct/**", "/api/Vol/ShowTenVolunteer","/api/Administrator/**")//放行的接口，应该填入无需登录也可以用的接口
+                .excludePathPatterns( "/**/*.html", "/**/*.js", "/**/*.css", "/**/*.woff", "/**/*.ttf","/swagger-ui")
+                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**","doc.html","/error"); // 放行静态文件
                 //.excludePathPatterns( "/**");
 
     }
