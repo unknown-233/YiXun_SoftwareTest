d4102e6a90152b3978db53e778f4ed19ec65320c
OOmiakon <1741264135@qq.com>
2023-06-11 22:46:04 +0800

    加了一个UnitExampleTest

---
+++ src/test/java/com/yixun/yixun_backend/unittest/JUnitExampleTest.java
@@ -0,0 +1,12 @@
+package com.yixun.yixun_backend.unittest;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class JUnitExampleTest {
+    @Test
+    public void demoTestMethod(){
+        Assertions.assertTrue(true);
+    }
+}
